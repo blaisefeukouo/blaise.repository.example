31b7212aef37ce1a9dcbf13b31ef4782cb50926d
Blaise Tofing Feukouo <feukouo.tofing@bao-sarl.com>
2015-03-02 10:37:26 +0100

    commit of class Person.java

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person{
+	import java.util.List;
+	private String name;
+	private Strng surName;
+	private long age;
+	
+	pulic Person(String name, Strng surName,age){
+		this.name=name;
+		this.surName=surName;
+		this.age=name;
+	}
+}
---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal{
+	import java.util.List;
+	private List<Person> personList=new ArrList<Person>
+	public static void main(String args[]){
+		personList.add(new Person("blaise","Feukouo",5));
+	}
+}
